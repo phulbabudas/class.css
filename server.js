f9a5b730ac130667c823cd999c8e74bfdef52001
phulbabudas <phulbabudas123@gmeil.com>
2024-09-12 16:34:11 +0530

    files added

---
+++ server.js
@@ -0,0 +1,11 @@
+const http=require ('http');
+const server=http.createServer((req,res)=>{
+    console.log('request has been node frome  browser to server');
+    console.log(req);
+})
+//port number ,host callback func
+server.listen(74,'localhost', ()=>{
+console.log('server is  listening on port 74');
+});
+
+    
