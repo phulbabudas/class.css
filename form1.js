995cd9dd484a886d624ca2acc8139aa5e3ae1cf9
phulbabudas <phulbabudas123@gmeil.com>
2024-09-12 16:44:32 +0530

    file added

---
+++ form1.js
@@ -0,0 +1,9 @@
+// document.getElementById("name").value="ram"
+// document.getElementById("password").value="1234"
+// document.getElementById("gmail").value="ram@gmail.com"
+let a = document.getElementById("a").value
+let b = document.getElementById("b").value
+let c = a+b
+document.getElementById("c").value= c
+console.log(a)
+console.log(b)
